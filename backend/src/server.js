7ca884052be9ac48e8f10b68af4a8be34e7f8706
Tim Misiak <tim.misiak+github@gmail.com>
2026-06-01 23:54:46 -0700

    Increase upload limit

--- backend/src/server.js
+++ backend/src/server.js
@@ -83,7 +83,7 @@ const __dirname = dirname(__filename);
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 // Serve built frontend
 app.use(express.static(join(__dirname, '../public')));
